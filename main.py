bef60c647f8b7b44c51acd849ec62046f1441863
jja42 <jja42w@gmail.com>
2025-06-07 14:59:35 -0400

    reformated word count and character count into report

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+from stats import get_text_words, get_text_characters, get_sorted_characters
+import sys
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def get_text_words(text):
-    words = text.split()
-    return len(words)
-
 
 def main():
-    path = "./books/frankenstein.txt"
+    if(len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     contents = get_book_text(path)
     #print(contents)
     word_count = get_text_words(contents)
-    print(f"{word_count} words found in the document")
+    character_count = get_text_characters(contents)
+    sorted_count = get_sorted_characters(character_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("----------- Character Count ----------")
+    for dict in sorted_count:
+        character = dict["key"]
+        if(character.isalpha()):
+            count = dict["value"]
+            print(f"{character}: {count}")
 
 
 
